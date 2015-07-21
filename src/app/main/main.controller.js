363c1ed1bea80c0b06983ab776bea28547fc17cc
James Muranga <jmured@gmail.com>
2015-07-22 00:19:15 +0530

    removed some unsed dependencies and setup the home page ui

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,39 +1,6 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('threebund')
-    .controller('MainController', MainController);
-
-  /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
-    var vm = this;
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1437486327247;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
-    }
-  }
-})();
+    angular
+      .module('threebund')
+      .controller('MainController', function MainController($timeout) {});});
