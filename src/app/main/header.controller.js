d3f9b5faa2c5557275eafef745e4be6c2d190f39
James Muranga <jmured@gmail.com>
2015-08-16 23:44:22 +0300

    cleaned up the unit tests for the header controller

--- src/app/main/header.controller.js
+++ src/app/main/header.controller.js
@@ -6,11 +6,15 @@
             var setTitle = function(title) {
                 $scope.title = title;
             };
+
             var setHomeUrl = function(url) {
                 $scope.homeUrl = url;
             };
+
             dhis.getApplicationTitle().then(setTitle);
+
             dhis.dhisUrl('').then(setHomeUrl);
+
             $scope.goHome = function() {
                 window.location = $scope.homeUrl;
             };
