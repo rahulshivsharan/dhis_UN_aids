04a3d7a827fae7ea7730bf3549feea87b4f63ea3
Rahul <rahul@duretechnologies.com>
2017-07-20 10:51:05 +0530

    Code formating

--- src/app/main/dhisService.js
+++ src/app/main/dhisService.js
@@ -7,7 +7,8 @@
 	function dhisService(DHIS_BACKEND,$http,$q){
 
 		var service = {};
-		service.getOrganisationUnits = getOrganisationUnits; 
+		service.getOrganisationUnits = getOrganisationUnits;
+		service.getDataElements = getDataElements; 
 
 		return service;
 
@@ -40,11 +41,48 @@
 				}	
 				
 				deferred.resolve(jsonResponseObj);
-			}
+			} // end of successFn
 
 			function errorFn(response){
 				deferred.reject(response);
-			}
+			} // end of errorFn
+		} // end of "getOrganisationUnits" 
+
+
+		function getDataElements(){
+			var url = DHIS_BACKEND + "/api/24/dataElements.json";
+			var deferred = $q.defer();
+			
+			$http({
+				"method" : "GET",
+				"url" : url
+			}).then(successFn,errorFn);
+
+			return deferred.promise;
+
+			function successFn(response){
+				
+				var jsonString = undefined,
+					jsonResponseObj = undefined;
+
+				// the below condition is added 
+				// to parse the response according to the endpoint configured.
+				// the first condition is for testing (i.e. DHIS_BACKEND pointing to testing or local environment) 
+				// where as the second condition is for production (i.e. DHIS_BACKEND pointing to production;
+				// when deployed in DHIS). 	
+				if("body" in response.data){					
+					jsonString = response.data.body;
+					jsonResponseObj = JSON.parse(jsonString);	
+				}else{					
+					jsonResponseObj = response.data
+				}	
+				
+				deferred.resolve(jsonResponseObj);
+			} // end of successFn
+
+			function errorFn(response){
+				deferred.reject(response);
+			} // end of errorFn
 		} // end of "getOrganisationUnits" 
 
 	} // end of dhisService
--- src/app/main/home.controller.js
+++ src/app/main/home.controller.js
@@ -4,23 +4,41 @@
     homeController.$inject = ["dhisService","$scope"]
 
     function homeController(dhisService,$scope){
-    	var vm = this;
-    	vm.init = init;
-    	vm.organisationUnits = undefined;
+    	console.log("Home Controller is intialised");   
+        var vm = this;
 
-    	console.log("Home Controller is intialised");
+        // public methods and variables
+    	vm.init = init;
+        vm.organisationUnits = undefined;
+        vm.dataElements = undefined; 	
+
+        // private method and variables
+        var getOrganisationUnits = getOrganisationUnits;
+        var getDataElements = getDataElements; 
 
     	function init(){
-    		var promise = dhisService.getOrganisationUnits();
-
-    		promise.then(function(response){
-    			vm.organisationUnits = response["organisationUnits"];
-    		},function(error){
-    			console.log(error);
-    		});
-
-
+    		getOrganisationUnits();
+            getDataElements();
     	}
 
+        function getOrganisationUnits(){
+            var promise = dhisService.getOrganisationUnits();
+
+            promise.then(function(response){
+                vm.organisationUnits = response["organisationUnits"];
+            },function(error){
+                console.log(error);
+            });
+        }
+
+        function getDataElements(){
+            var promise = dhisService.getDataElements();
+
+            promise.then(function(response){
+                vm.dataElements = response["dataElements"];
+            },function(error){
+                console.log(error);
+            });
+        }
     } // end of "homeController" 
 })();
